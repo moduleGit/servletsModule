7514246f1d1148a8575ba8f14c4426c4eabcf555
chris <chrisnevelsteen@gmail.com>
2015-03-31 16:13:28 +0200

    p56

--- pizzaluigi/src/be/vdab/servlets/HeadersServlet.java
+++ pizzaluigi/src/be/vdab/servlets/HeadersServlet.java
@@ -1,7 +1,9 @@
 package be.vdab.servlets;
 
 import java.io.IOException;
+import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import javax.servlet.ServletException;
@@ -12,27 +14,36 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/headers.htm")
 public class HeadersServlet extends HttpServlet {
-private static final long serialVersionUID = 1L;
-private static final String VIEW = "/WEB-INF/JSP/headers.jsp";
-private final Map<String, String> browsers = new HashMap<>(); //(1)
-public HeadersServlet() {
-browsers.put("firefox", "Firefox");
-browsers.put("chrome", "Chrome");
-browsers.put("msie", "Internet Explorer");
-browsers.put("trident", "Internet Explorer");
-}
-@Override
-protected void doGet(HttpServletRequest request, HttpServletResponse response)
-throws ServletException, IOException {
-String userAgent = request.getHeader("user-agent").toLowerCase(); //(2)
-String browser = "onbekend"; //(3)
-for (Map.Entry<String, String> entry : browsers.entrySet()) { //(4)
-if (userAgent.contains(entry.getKey())) { //(5)
-browser = entry.getValue();
-break; //(6)
-}
-}
-request.setAttribute("browser", browser);
-request.getRequestDispatcher(VIEW).forward(request, response);
-}
+	private static final long serialVersionUID = 1L;
+	private static final String VIEW = "/WEB-INF/JSP/headers.jsp";
+
+	/*
+	 * private final Map<String, String> browsers = new HashMap<>(); // (1)
+	 * 
+	 * public HeadersServlet() { browsers.put("firefox", "Firefox");
+	 * browsers.put("chrome", "Chrome"); browsers.put("msie",
+	 * "Internet Explorer"); browsers.put("trident", "Internet Explorer"); }
+	 * 
+	 * @Override protected void doGet(HttpServletRequest request,
+	 * HttpServletResponse response) throws ServletException, IOException {
+	 * String userAgent = request.getHeader("user-agent").toLowerCase(); // (2)
+	 * String browser = "onbekend"; // (3) for (Map.Entry<String, String> entry
+	 * : browsers.entrySet()) { // (4) if (userAgent.contains(entry.getKey())) {
+	 * // (5) browser = entry.getValue(); break; // (6) } }
+	 * request.setAttribute("browser", browser);
+	 * request.getRequestDispatcher(VIEW).forward(request, response); }
+	 */// alleen browserinfo opvragen
+
+	@Override
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		Map<String, String> headers = new LinkedHashMap<>();
+		for (Enumeration<String> headerNames = request.getHeaderNames(); headerNames
+				.hasMoreElements();) {
+			String headerName = headerNames.nextElement();
+			headers.put(headerName, request.getHeader(headerName));
+		}
+		request.setAttribute("headers", headers);
+		request.getRequestDispatcher(VIEW).forward(request, response);
+	}
 }
